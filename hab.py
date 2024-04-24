0a43bb51023cf40ed038d632f4756892e2068806
JavAlex17 <72239869+JavAlex17@users.noreply.github.com>
2024-04-24 01:31:23 -0400

    Main y habitacion

--- hab.py
+++ hab.py
@@ -3,27 +3,79 @@ from PyQt6.QtWidgets import *
 from PyQt6.QtGui import *
 from PyQt6.QtCore import *
 
+
 class VentanaHabitaciones(QDialog):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("Habitaciones")
         self.setStyleSheet("background-color: #fefeff;")
-        self.setMinimumSize(600, 500)
+        self.setMinimumSize(590, 560)
+    
+        
+        #Agregar una Font personalizada
+        font_local = "assets/fonts/PPNeueMachina-PlainRegular.otf"
+        font_id = QFontDatabase.addApplicationFont(font_local)
+        font = QFontDatabase.applicationFontFamilies(font_id)[0]
         
         layout = QVBoxLayout(self)
         
+        
+        #Agregar una appBar y el boton de menu a la izquierda
+        appbar_layout = QGridLayout()
+        appbar_layout.setContentsMargins(20, 10, 20, 10)
+        
+        
+        self.botonMenu = QLabel(self)
+        pixmap_btnmenu = QPixmap("images/menuappbar.png").scaledToWidth(35)
+        self.botonMenu.setPixmap(pixmap_btnmenu)
+        self.botonMenu.setAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignVCenter)
+        #Lambda event: es para ignorar el evento de moussePressEvent, pero que igualmente se llame a la funcion atras
+        #self.botonMenu.mousePressEvent = lambda event: self.abrirMenu()
+    
+        appbar_layout.addWidget(self.botonMenu,0,0)
+        
+        #Titulo de la appBar a la derecha
+        tituloHotel = QLabel("Hotel CTCh")
+        tituloHotel.setStyleSheet('color: #686961;')
+        tituloHotel.setFont(QFont(font, 20))
+        tituloHotel.setAlignment(Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignVCenter)
+        appbar_layout.addWidget(tituloHotel,0,1)
+        
+        #Logo del hotel a la derecha de la appBar
+        logo_appbar = QLabel()
+        pixmap_logo = QPixmap("images/logoappbar.png").scaledToWidth(45)
+        logo_appbar.setPixmap(pixmap_logo)
+        logo_appbar.setAlignment(Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignVCenter)
+        appbar_layout.addWidget(logo_appbar,0,2)
+        
+        #appbar como widget para agregarle color
+        appbarWidget = QWidget()
+        appbarWidget.setLayout(appbar_layout)
+        appbarWidget.setStyleSheet("background-color: #BAB78D;")
+        appbarWidget.setFixedHeight(65)
+        
+        layout.addWidget(appbarWidget)
+        
+        layout_titulo = QVBoxLayout()
         # Título
-        title_label = QLabel("Habitaciones")
-        title_label.setStyleSheet("color: #686961; font-style: italic")
-        title_label.setFont(QFont("Segoe UI", 27, QFont.Weight.Bold))
-        title_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        layout.addWidget(title_label)
+        self.tituloHab = QLabel("Habitaciones")
+        self.tituloHab.setStyleSheet("color: #686961; font-style: italic")
+        self.tituloHab.setFont(QFont(font, 27, QFont.Weight.Bold))
+        self.tituloHab.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        
+        layout_titulo.addWidget(self.tituloHab)
+        
+        tituloWidget = QWidget()
+        tituloWidget.setLayout(layout_titulo)
+        tituloWidget.setFixedHeight(60)
+        
+        layout.addWidget(tituloWidget)
         
         #Listas habitaciones
-        self.ListaImagenes = ["images/hab1.jpg", "images/hab2.jpg", "images/hab3.jpg", "images/hab4.jpg", "images/hab5.jpg"]
+        self.ListaImagenes = ["images/hab1.png", "images/hab2.jpg", "images/hab3.jpg", "images/hab4.jpg", "images/hab5.jpg"]
         self.ListaDisponibilidad = [10,10,10,1,1]
-        self.ListaNombres = ["Habitación Ejecutiva Individual", "Habitación Ejecutiva Doble", "Habitación Familiar", "PentHouse Volcanes", "PentHouse Pacifico"]
+        self.ListaNombres = ["Habitación Ejecutiva Individual", "Habitación Ejecutiva Doble", "Habitación Familiar", "PentHouse Volcanes", "PentHouse Pacífico"]
         self.ListaPrecios = ["$50.000", "$80.000", "$150.000", "$1.080.000", "$1.080.000"]
         self.ListaCapacidad = ["2 (1 Recomendado)", "4 (2 Recomendado)", "8 (6 Recomendado)", "2 (Invitados temporales indefinidos)", "2 (Invitados temporales indefinidos)"]
         self.ListaDestacados = ["Ninguno.", "Ninguno.", "Habitación matrimonial y<br>habitación con camarotes dobles.", "Terraza al aire libre, habitación<br>principal y habitación de invitados.", "Terraza al aire libre, habitación<br>principal y habitación de invitados."]
@@ -33,6 +85,7 @@ class VentanaHabitaciones(QDialog):
         
         #Layout horizontal para la imagen y los botones
         hbox = QHBoxLayout()
+        hbox.setContentsMargins(0, 0, 0, 5)
         
         #Boton retroceder
         self.boton_atras = QLabel(self)
@@ -60,24 +113,29 @@ class VentanaHabitaciones(QDialog):
         
         layout.addLayout(hbox)
 
+    
         
         # Información debajo de la imagen
         self.info_label = QLabel()
         self.actualizar_info()
         self.info_label.setStyleSheet("color: #686961;")
-        self.info_label.setFont(QFont("Segoe UI", 12))
+        self.info_label.setFont(QFont(font, 12))
         self.info_label.setAlignment(Qt.AlignmentFlag.AlignLeft)
         layout.addWidget(self.info_label)
         
+        texto = QLabel(
+            "<div style='margin-left: 45px; font-size: 10pt; line-height: 1.2;'>*Todas las reservas incluyen desayuno continental para todos los pasajeros.</div>")
+        texto.setAlignment(Qt.AlignmentFlag.AlignBottom)
+        layout.addWidget(texto)
         
     #Funcion para actualizar la informacion
     #Informacion estilizada con html
     def actualizar_info(self):
-        nombre_habitacion = f"<div style='color: #686961; font-size: 16pt; margin-left: 20px; font-style: italic; line-height: 1.15;'>{self.ListaNombres[self.indice]}</div>"
-        capacidad_personas = f"<div style='margin-left: 50px; font-size: 12pt; line-height: 1.15;'>Capacidad de Personas: {self.ListaCapacidad[self.indice]}</div>"
-        servicios_destacados = f"<div style='margin-left: 50px; font-size: 12pt; line-height: 1.15;'>Servicios destacados: {self.ListaDestacados[self.indice]}</div>"
-        texto = "<div style='margin-left: 50px; font-size: 12pt; line-height: 1.15;'>Todas las reservas incluyen desayuno continental para todos los pasajeros.</div>"
-        info = f"{nombre_habitacion}{capacidad_personas}{servicios_destacados}{texto}"
+        nombre_habitacion = f"<div style='color: #686961; font-size: 16pt; margin-left: 20px; font-style: italic; line-height: 1.5;'>{self.ListaNombres[self.indice]} - {self.ListaPrecios[self.indice]}</div>"
+        disponibilidad = f"<div style='margin-left: 45px; font-size: 12pt; line-height: 1.2;'>Disponibilidad: {self.ListaDisponibilidad[self.indice]}</div>"
+        capacidad_personas = f"<div style='margin-left: 45px; font-size: 12pt; line-height: 1.2;'>Capacidad de Personas: {self.ListaCapacidad[self.indice]}</div>"
+        servicios_destacados = f"<div style='margin-left: 45px; font-size: 12pt; line-height: 1.2;'>Servicios destacados: {self.ListaDestacados[self.indice]}</div>"
+        info = f"{nombre_habitacion}{disponibilidad}{capacidad_personas}{servicios_destacados}"
         self.info_label.setText(info)
         
     #Funcion botones   
--- maincod.py
+++ maincod.py
@@ -10,17 +10,23 @@ class MainWin(QMainWindow):
         self.setWindowTitle('Hotel CTCh')
         self.setStyleSheet("background-color: #fefeff;")
         
+        #Agregar una Font personalizada
+        font_local = "assets/fonts/PPNeueMachina-PlainRegular.otf"
+        font_id = QFontDatabase.addApplicationFont(font_local)
+        font = QFontDatabase.applicationFontFamilies(font_id)[0]
+        
         self.central_widget = QWidget()
         self.setCentralWidget(self.central_widget)
-        self.setMinimumSize(600, 500)
-        self.initUI()
+        self.setMinimumSize(590, 560)
+        self.initUI(font)
         
-    def initUI(self):
+    def initUI(self, font):
         layout = QVBoxLayout(self.central_widget)
+        layout.setContentsMargins(0, 20, 0, 5)
         
         #Imagen
         image_label = QLabel(self)
-        pixmap = QPixmap("images/logo.png").scaledToWidth(200)  # Ruta de la imagen
+        pixmap = QPixmap("images/logo.png").scaledToWidth(180)  # Ruta de la imagen
         image_label.setPixmap(pixmap)
         image_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         layout.addWidget(image_label)
@@ -28,18 +34,22 @@ class MainWin(QMainWindow):
         #Titulo de bienvenida
         welcome_label = QLabel("¡Bienvenido al Hotel CTCh!")
         welcome_label.setStyleSheet("color: #686961; font-style: italic; font-weight: bold")
-        welcome_label.setFont(QFont("Segoe UI", 27))
+        welcome_label.setFont(QFont(font, 25))
         welcome_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         layout.addWidget(welcome_label)
         
         #Subtitulo estilo html
         sub_label = QLabel(
-            "<span style='color: #686961; font-size: 13pt; line-height: 1.2;'>"
-            "Un espacio de descanso y relajación, con multiples  <br>"
-            "instalaciones y actividades enfocadas en la reconección con la naturaleza <br>"
-            " con todo el lujo y a comodidad de un hotel 5 estrellas."
-            "</span>")
+            "<div style='color: #686961; line-height: 1.25;'>"
+            "Un espacio de descanso y relajación, <br>"
+            "con multiples instalaciones y<br>"
+            "actividades enfocadas en la<br>"
+            "reconexión con la naturaleza con<br>"
+            "todo el lujo y la comodidad de un<br>"
+            "hotel 5 estrellas."
+            "</div>")
         sub_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        sub_label.setFont(QFont(font, 16))
         sub_label.setWordWrap(True)
         layout.addWidget(sub_label)
         
@@ -47,11 +57,10 @@ class MainWin(QMainWindow):
         #Boton de entrar
         botonEntrar = QPushButton("Entrar")
         botonEntrar.setStyleSheet("background-color: #BAB78D; color: #686961;")
-        botonEntrar.setFont(QFont("Segoe UI", 15))
+        botonEntrar.setFont(QFont(font, 15))
         botonEntrar.setFixedSize(140,40)  # Ancho máximo del botón
         layout.addWidget(botonEntrar, alignment=Qt.AlignmentFlag.AlignCenter)  # Centrar el botón en el layout
 
-        #botonEntrar.clicked.connect(VentanaHabitaciones.abrirVentanaHabitaciones)
         botonEntrar.clicked.connect(self.abrirVentanaHabitaciones)
         
     def abrirVentanaHabitaciones(self):
